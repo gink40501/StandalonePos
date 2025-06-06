4ee5d9cedce6ff51dac6a069d4dc95fbd21efa0e
jim <gink40501@gmail.com>
2025-06-06 13:24:17 +0800

    feat:新增報表的表單

---
+++ UI/PeriodicReports.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 一盒蛋玩具店
+{
+    public partial class PeriodicReports : Form
+    {
+        public PeriodicReports()
+        {
+            InitializeComponent();
+        }
+    }
+}
